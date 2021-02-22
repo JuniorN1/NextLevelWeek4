850100904457c3993e5236b127e81816f1f80915
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-22 10:20:53 -0300

    add method get to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express';
 
 const app = express();
+
+app.get("/users",(request,response)=>{
+    return response.json({message:"Hello World - NLW04"});
+    // return response.send("Hello World - NLW04");
+});
 app.listen(3333,()=>console.log("Server is running!"));
+
 
