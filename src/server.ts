b276646d8828c1840d19e95f826bf913826a7077
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-22 10:21:41 -0300

    modify method get to server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get("/users",(request,response)=>{
+app.get("/",(request,response)=>{
     return response.json({message:"Hello World - NLW04"});
-    // return response.send("Hello World - NLW04");
 });
+
 app.listen(3333,()=>console.log("Server is running!"));
 
 
