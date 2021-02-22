9989cd3a1e4df3ec72543e724ffea5ce5b1d0535
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-22 10:34:06 -0300

    add method post server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express();
 
 app.get("/",(request,response)=>{
     return response.json({message:"Hello World - NLW04"});
+});
+app.post("/",(request,response)=>{
+    return  response.json({message:"Sucess in save data!"})
 });
 
 app.listen(3333,()=>console.log("Server is running!"));
