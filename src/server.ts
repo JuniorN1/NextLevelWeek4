923bfa6d9350ffe0b474dd3b6952087557f85276
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-23 10:27:18 -0300

    routes get json

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import './database';
 import { router } from './router';
 const app = express();
-
+app.use(express.json())
 app.use(router);
 
 app.listen(3333,()=>console.log("Server is running!"));
