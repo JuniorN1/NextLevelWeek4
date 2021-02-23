d1fb537de407eb64c6c1900b374d3325989f47de
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-23 09:53:33 -0300

    config to table user

--- src/database/migrations/1614084299983-CreateUser.ts
+++ src/database/migrations/1614084299983-CreateUser.ts
@@ -1,11 +1,42 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUser1614084299983 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table(
+                {
+                    name: "users",
+                    columns:[
+                        {
+                            name:"id",
+                            type:"uuid",
+                            isPrimary:true
+
+                        },
+                        {
+                            name:"name",
+                            type:"varchar"
+
+                        },
+                        {
+                            name:"email",
+                            type:"varchar"
+                        },
+                        {
+                            name:"created_at",
+                            type:"tumestamp",
+                            default:"now()"
+                        }
+                    ]
+                }
+            )
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+
     }
 
 }
