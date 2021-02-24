9fbb053117be3f9eed545c54f377242b972c2d4a
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-24 12:44:59 -0300

    add status response 201 in users controllers and config testes to new user in database

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,13 +1,22 @@
 import request from 'supertest'
 import { app } from '../app'
-
-describe("User",()=>{
-    request(app)
-    .post("/users")
-    .send(
-        {
-            email:"Alou@gmail.com",
-            name:"User name"
-        }
-    )
-})
+import createConnection from '../database';
+describe("User", ()=>{
+    beforeAll(async()=>{
+        const connection =await createConnection();
+        await connection.runMigrations();
+    })
+    it("Should be able to create a new user",async ()=>{
+        const response = await request(app)
+        .post("/users")
+        .send(
+            {
+                email:"Alou@gmail.com",
+                name:"User name"
+            }
+        );
+        expect(response.status).toBe(201)
+    });
+   
+  
+});
