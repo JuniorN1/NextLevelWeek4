482b2dc8aa400a36a5fbe916fef64f81829d546c
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-24 10:34:24 -0300

    Create migration surveys

---
+++ src/database/migrations/1614173115696-CreateSurveys.ts
@@ -0,0 +1,45 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSurveys1614173115696 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table(
+                {
+
+                    name:"surveys",
+                    columns:[                    
+                        {  
+                            name:"id",                        
+                            type :"uuid",                        
+                            isPrimary:true
+                        },
+                        {
+                            name:"title",
+                            type:"varchar",
+
+                        },
+                        {
+                            name:"description",
+                            type:"varchar"
+                        },
+                        {
+                            name:"creates_at",
+                            type:"timestemp",
+                            default:"now()"
+                        }
+                        
+
+                    ]
+                }
+               
+            )
+        )
+    }
+    
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("surveys")
+    }
+
+}
