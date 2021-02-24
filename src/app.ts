2b730ec6a72559e17baef21a2b83ce9d9ecc0faf
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-24 12:38:06 -0300

    config app.ts to call createConnecton to database

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
 import express from 'express';
-import './database';
+import createConnection from './database';
 import { router } from './router';
+createConnection();
 const app = express();
 app.use(express.json())
 app.use(router);
-export{app}
+export{app} 
