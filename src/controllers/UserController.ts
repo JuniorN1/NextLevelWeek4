c265536f9f7b63a9f63781cb4243de5771c1d7b9
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-24 10:22:29 -0300

    remover importes not useds

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,7 @@
-import {Request,Response} from "express";
+import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
-import {UsersRepository} from "../repositories/UsersRepository"
 class UserController{
 
      async create(request:Request,response:Response){
@@ -33,4 +33,4 @@ class UserController{
 }
 
 
-export { UserController }
+export { UserController };
