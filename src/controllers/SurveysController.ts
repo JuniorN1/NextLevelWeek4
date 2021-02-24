8e67e1c04f2229d2e456291131ff12553c0d4b1a
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-24 11:09:49 -0300

    create show method in surveys

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -4,7 +4,7 @@ import { SurveysRepository } from "../repositories/SurveysRepository";
 class SurveysController{
     async create(request:Request,response:Response){
         const {title, description} = request.body;
-        const surveysRepository =getCustomRepository(SurveysRepository)
+        const surveysRepository =getCustomRepository(SurveysRepository);
 
         const survey = surveysRepository.create({
             title,
@@ -12,6 +12,11 @@ class SurveysController{
         })
         await surveysRepository.save(survey);
         return response.status(201).json(survey);
+    }
+    async show(request:Request,response:Response) {
+        const surveysRepository =getCustomRepository(SurveysRepository);
+        const all = await surveysRepository.find()
+        return response.json(all)
     }
 
 }
