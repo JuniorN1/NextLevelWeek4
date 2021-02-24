10614417bdf96053c55225f073f1fd5820c58fbc
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-24 13:48:26 -0300

    last tests , rm not execult in windows

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ export default async(): Promise<Connection> =>{
                 database:process.env.NODE_ENV === 'test'?
                 "./src/database/database.test.sql"
                 :
-               ""
+                defaultOptions.database
             }
         )
 
