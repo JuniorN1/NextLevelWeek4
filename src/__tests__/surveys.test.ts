6ffd40420917fb13b999cadc09e2e8056cf6ace8
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-24 13:39:44 -0300

    create file tests surveys and configs test

--- src/__tests__/surveys.test.ts
+++ src/__tests__/surveys.test.ts
@@ -1,33 +1,36 @@
 import request from 'supertest'
 import { app } from '../app'
 import createConnection from '../database';
-describe("User", ()=>{
+describe("Surveys", ()=>{
     beforeAll(async()=>{
         const connection =await createConnection();
         await connection.runMigrations();
     })
-    it("Should be able to create a new user",async ()=>{
+    it("Should be able to create a new surveys",async ()=>{
         const response = await request(app)
-        .post("/users")
+        .post("/surveys")
         .send(
             {
-                email:"Alou@gmail.com",
-                name:"User name"
+                title:"Alou@gmail.com",
+                description:"User name"
             }
         );
         expect(response.status).toBe(201);
         expect(response.body).toHaveProperty("id");
     });
-   it("should not be able to create a user if with exist email",async ()=>{
-        const response = await request(app)
-        .post("/users")
+   it("should  to get all surveys",async ()=>{
+        await request(app)
+        .post("/surveys")
         .send(
             {
-                email:"Alou@gmail.com",
-                name:"User name"
+                title:"Alou@gmail.com",
+                description:"User name"
             }
         );
-        expect(response.status).toBe(400);
+        const response = await request(app)
+        .get("/surveys");
+        expect(response.body.length).toBe(2)
+        
 
     });
     
