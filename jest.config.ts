0e243cb61f4b935827f1fe406e39bf25279725c3
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-24 11:38:10 -0300

    testMatch changer folder testes

--- jest.config.ts
+++ jest.config.ts
@@ -147,8 +147,7 @@ export default {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[tj]s?(x)"
+    "**/__tests__/*.test.ts"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
