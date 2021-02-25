2ba8f8d3d8721f8278605f9d6ffc0329fdcc2d11
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-25 12:56:48 -0300

    create services SendMail

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,9 +1,32 @@
-import nodemailer from 'nodemailer';
+import nodemailer, {  Transporter } from 'nodemailer';
 class SendMailService{
+    private client:Transporter
     constructor(){
-        nodemailer.createTestAccount
+        nodemailer.createTestAccount().then(account=>{
+            let transporter = nodemailer.createTransport({
+                host: account.smtp.host,
+                port: account.smtp.port,
+                secure: account.smtp.secure, // true for 465, false for other ports
+                auth: {
+                  user: account.user, // generated ethereal user
+                  pass: account.pass, // generated ethereal password
+                },
+              });
+            this.client = transporter;
+        })
     }
-    async execute(){
+    async execute(to:string,subject:string,body:string){ 
+        const message = await this.client.sendMail(
+            {
+                to,
+                subject,
+                html:body,
+                from:"NPS <norelay@nps.com.br>"
+            }
+        )
+        console.log("Message sent: %s", message.messageId);
+ 
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
 
     }
 
