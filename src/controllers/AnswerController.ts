0fdf5b290b19a1cc3531e2c0f6166b1a8a760731
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-26 10:16:05 -0300

    save avaliable assessment answerconttroler in database

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveyUser } from "../model/SurveyUser";
+import { SurveysUserRepository } from "../repositories/SurveysUserRepository";
 class AnswerController{
     
     async execute(request:Request,response:Response){
-    
+        const {value} = request.params;
+        const {u}   =request.query;
 
+      
+        const surveysUserRepository = getCustomRepository(SurveysUserRepository);
+        const surveyUser = await surveysUserRepository.findOne(
+            {
+                id:String(u),
+
+            }
+        );
+        if(!surveyUser){
+            return response.status(400).json({
+                error : "Survey User des not exists!"
+            })
+        }
+        surveyUser.value= Number(value);
+
+        await surveysUserRepository.save(SurveyUser);
+        return response.status(201).json(surveyUser)
 
     }
 }
