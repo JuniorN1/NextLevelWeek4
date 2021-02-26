f35d3339b9a27a68324d87e472251c83be801175
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-26 12:38:28 -0300

    import AppError in to userControllercls

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction,Request,Response } from 'express';
 import createConnection from './database';
 import { router } from './router';
+import { AppError } from './errors/AppError';
 createConnection();
 const app = express();
 app.use(express.json())
 app.use(router);
+app.use((err:Error,request:Request,response:Response,_next:NextFunction)=>{
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            message:err.message});
+    } 
+
+})
 export{app} 
