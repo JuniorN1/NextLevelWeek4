a1485d159142c6b34e4c09b9edc9a14f9687a050
JuniorN1 <junior_e.a.o@hotmail.com>
2021-02-26 12:13:24 -0300

    add validation in create user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
-
+import * as yup from 'yup';
 class UserController{
 
      async create(request:Request,response:Response){
@@ -10,6 +10,15 @@ class UserController{
             email
             
         }=request.body;
+        const schema = yup.object().shape({
+            name:yup.string().required(),
+            email:yup.string().email().required(),
+        })
+        if(!(await schema.isValid(request.body))){
+            return response.status(400).json({
+                error:"Validation Failed!"
+            })
+        }
         const userRepository = getCustomRepository(UsersRepository);
         const userAlreadyExists = await userRepository.findOne({
             email
